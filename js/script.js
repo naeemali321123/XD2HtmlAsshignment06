8afc67d227386ba7d9707731f0766dc79d21bdea
naeemali321123 <naeemali321123@gmail.com>
2021-11-30 16:23:23 +0500

    inicial commit

---
+++ js/script.js
@@ -0,0 +1,62 @@
+function sound() {
+    var audio = new Audio('../mp3/click.mp3');
+    audio.play();
+}
+
+function sendBtnValue(btnValue) {
+    document.getElementById("currentOperand").innerHTML += btnValue;
+    sound();
+}
+
+function allClear() {
+    document.getElementById("currentOperand").innerHTML = null;
+    document.getElementById("previousOperand").innerHTML = null;
+    sound();
+}
+
+function plus() {
+    var plusSign = "+";
+    document.getElementById("currentOperand").innerHTML += plusSign;
+    sound();
+}
+
+function minus() {
+    var minusSign = "-";
+    document.getElementById("currentOperand").innerHTML += minusSign;
+    sound();
+}
+
+function multiply() {
+    var multiplySign = "*";
+    document.getElementById("currentOperand").innerHTML += multiplySign;
+    sound();
+}
+
+function devide() {
+    var devideSign = "/";
+    document.getElementById("currentOperand").innerHTML += devideSign;
+    sound();
+}
+
+function point() {
+    var pointSign = ".";
+    document.getElementById("currentOperand").innerHTML += pointSign;
+    sound();
+}
+
+function equal() {
+    sound();
+    var screenData = document.getElementById("currentOperand").innerHTML;
+    var result = document.getElementById("currentOperand").innerHTML = eval(screenData);
+    var equalSign = " = "
+    document.getElementById("currentOperand").innerHTML = result;
+    document.getElementById("previousOperand").innerHTML = screenData += ' = '
+    
+}
+
+function deleteData() {
+    var screenValue = document.getElementById("currentOperand").innerHTML;
+    var deleteOne = screenValue.slice(0, -1);
+    document.getElementById("currentOperand").innerHTML = deleteOne ;
+    sound();
+}
